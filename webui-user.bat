16b0c75da1b29d76f88a7ef98278035cb46340ea
chipsxp <chips_xp@yahoo.com>
2024-01-31 11:55:27 -0500

    updates
    python updates 3.11

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,30 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-git pull
+
+echo Please select an option:
+
+echo 1) xformers                        (Fastest)
+echo 2) xformers + medvram              (Slower)
+echo 3) xformers + lowvram              (Slowest)
+echo 4) Update                          (git pull, reinstall xformers, reinstall torch)
+
+choice /C 1234 /N /M "Choice (1-4): "
+
+if errorlevel 4 (
+    echo Updating...
+    set COMMANDLINE_ARGS= --reinstall-xformers --reinstall-torch --xformers
+    call git pull 
+	python -m pip install --upgrade pip
+) else if errorlevel 3 (
+    echo Starting...
+    set COMMANDLINE_ARGS= --xformers --lowvram
+) else if errorlevel 2 (
+    echo Starting...
+    set COMMANDLINE_ARGS= --xformers --medvram
+) else if errorlevel 1 (
+    echo Starting...
+    set COMMANDLINE_ARGS= --xformers
+)
+
 call webui.bat
